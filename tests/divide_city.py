929e1028bb339127c30aead0cb7ee6f5008b5442
AJBuilder <adam.aj.robertson@gmail.com>
2025-02-17 22:17:26 -0500

    Working on gui loading

--- tests/divide_city.py
+++ tests/divide_city.py
@@ -1,28 +1,20 @@
 '''Test workspace to try and split a city image into tile images'''
 import cv2
-
-# Load the image (provide the correct path)
-image = cv2.imread("images/image.jpg")
-
-# Check if the image was loaded successfully
-if image is None:
-    print("Error: Could not load image.")
-else:
-    # Display the image
-    cv2.imshow("Loaded Image", image)
-    
-    # Wait for a key press and close the window
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-
-
-
-
-
+import os
 
 
 if __name__ == "__main__":
-    
-    pass
+    # Load the image (provide the correct path)
+    path = os.path.join(os.path.dirname(__file__), "images/city1.png")
+    image = cv2.imread(path)
+
+    # Check if the image was loaded successfully
+    if image is None:
+        print("Error: Could not load image.")
+    else:
+        # Display the image
+        cv2.imshow("Loaded Image", image)
+        
+        # Wait for a key press and close the window
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()

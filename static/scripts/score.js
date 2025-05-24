e5c0156dc40d9f4d706ca3b055e9991fdb270636
hemmerlinaj20 <ajhemm03@gmail.com>
2025-05-24 15:04:37 -0400

    I think I fixed a minor bug in civic scoring.

--- static/scripts/score.js
+++ static/scripts/score.js
@@ -231,7 +231,7 @@ function scoreCivics(cityTiles){
         for(let j = 0; j < cityTiles[i].length; j++){
             const tile = cityTiles[i][j];
             if(tile.split(" ")[0] === "civic"){
-                let this_civic = 0;
+                let this_civic = 1;
                 const b1 = tile.split(" ")[1];
                 const b2 = tile.split(" ")[2];
                 const neg = tile.split(" ")[3];
@@ -240,7 +240,6 @@ function scoreCivics(cityTiles){
                     let b1_found = false;
                     let b2_found = false;
                     if(neighbor.split(" ")[0] === neg){
-                        this_civic = 1;
                         break;
                     }
                     if(b1_found == false && neighbor.split(" ")[0] === b1){
